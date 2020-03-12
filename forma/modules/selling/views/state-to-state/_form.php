81161352db5f21957b03a05494f7d15152f889ce
truemw88 <truemw88@gmail.com>
2020-03-12 19:16:35 +0200

    Убрал лишний инпут

--- forma/modules/selling/views/state-to-state/_form.php
+++ forma/modules/selling/views/state-to-state/_form.php
@@ -14,10 +14,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'state_id')->textInput() ?>
 
-    <?= $form->field($model, 'to_state_id')->textInput() ?>
-
-<!--    --><?//= $form->field($model, 'toState.name')->textInput() ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
