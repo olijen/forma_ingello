4689526eeb1a14e57cdef917b3cce25ed2a534b0
Oleg Grigoryev <olijenius@gmail.com>
2020-03-13 14:06:05 +0200

    Глобальная база данных

--- forma/config/db.php
+++ forma/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=dbforma;dbname=warehouse',
-    'username' => 'user',
-    'password' => 'pwd',
+    'dsn' => 'mysql:host=forma.ingello.com;dbname=warehouse;port=3301',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
