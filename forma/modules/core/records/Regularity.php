6b9816945f5ec6ac9962ed3c33e54c40218b19bc
truemw88 <truemw88@gmail.com>
2020-03-17 10:33:26 +0200

    Добавил регламент

---
+++ forma/modules/core/records/Regularity.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace forma\modules\core\records;
+
+use Yii;
+
+/**
+ * This is the model class for table "regularity".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $user_id
+ * @property int $order
+ *
+ * @property User $user
+ * @property RegularityItem[] $regularityItems
+ */
+class Regularity extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'regularity';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'user_id'], 'required'],
+            [['user_id', 'order'], 'integer'],
+            [['name'], 'string', 'max' => 55],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'user_id' => Yii::t('app', 'User ID'),
+            'order' => Yii::t('app', 'Order'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id'])->inverseOf('regularities');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['regularity_id' => 'id'])->inverseOf('regularity');
+    }
+}
---
+++ forma/modules/core/views/regularity/update.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use \wokster\ltewidgets\BoxWidget;
+
+/* @var $this yii\web\View */
+/* @var $model forma\modules\core\records\Regularity */
+
+$this->title = Yii::t('app', 'Редактировать Регламент: ', [
+    'modelClass' => 'Regularity',
+]) . ' ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Регламенты'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Редактировать');
+?>
+<div class="regularity-update">
+
+    <?php BoxWidget::begin([
+    'title'=>'Регламент: форма редактирования',
+    ]);
+    ?>
+
+    <?= $this->render('_form', [
+    'model' => $model,
+    ]) ?>
+
+    <?php BoxWidget::end();?>
+
+</div>
