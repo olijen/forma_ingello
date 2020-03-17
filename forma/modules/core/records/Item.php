2ef9fdd9e2585e031a8d25311edceb18cbeb6ac3
truemw88 <truemw88@gmail.com>
2020-03-17 10:33:26 +0200

    Добавил регламент

---
+++ forma/modules/core/records/Item.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace forma\modules\core\records;
+
+use Yii;
+
+/**
+ * This is the model class for table "item".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property int $parent_id
+ * @property int $regularity_id
+ * @property int $order
+ * @property string $color
+ *
+ * @property Regularity $regularity
+ * @property Item $parent
+ * @property Item[] $items
+ */
+class Item extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'item';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'regularity_id', 'order'], 'integer'],
+            [['title', 'description'], 'string', 'max' => 255],
+            [['color'], 'string', 'max' => 55],
+            [['regularity_id'], 'exist', 'skipOnError' => true, 'targetClass' => Regularity::className(), 'targetAttribute' => ['regularity_id' => 'id']],
+            [['parent_id'], 'exist', 'skipOnError' => true, 'targetClass' => Item::className(), 'targetAttribute' => ['parent_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'description' => Yii::t('app', 'Description'),
+            'parent_id' => Yii::t('app', 'Parent ID'),
+            'regularity_id' => Yii::t('app', 'Regularity ID'),
+            'order' => Yii::t('app', 'Order'),
+            'color' => Yii::t('app', 'Color'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRegularity()
+    {
+        return $this->hasOne(Regularity::className(), ['id' => 'regularity_id'])->inverseOf('items');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParent()
+    {
+        return $this->hasOne(Item::className(), ['id' => 'parent_id'])->inverseOf('items');
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['parent_id' => 'id'])->inverseOf('parent');
+    }
+}
