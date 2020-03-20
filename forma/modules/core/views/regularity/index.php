960635b8fff7bccaccdacc4a8bccaab4656ec8c2
truemw88 <truemw88@gmail.com>
2020-03-20 17:07:27 +0200

    Вывел регламенты

--- forma/modules/core/views/regularity/index.php
+++ forma/modules/core/views/regularity/index.php
@@ -3,7 +3,7 @@
 //de($regularity =$dataProvider->getModels()[0]);
 $regularitys = $dataProvider->getModels();
 //de($regularitys[1]->items);
-
+//$data = [];
 ?>
 <section class="content-header">
     <h1 align="center">
@@ -26,145 +26,62 @@ $regularitys = $dataProvider->getModels();
         </ul>
 
 
-<div class="tab-content">
+        <div class="tab-content">
 
-<!--            --><?php //foreach ($regularitys as $regularity): ?>
-<!--                <div class="tab-pane --><?//= $regularity['order'] == 1 ? 'active' : '' ?><!--"-->
-<!--                     id="tab_--><?//= $regularity['id'] ?><!--">-->
-<!--                    --><?php //foreach ($regularity->items as $item): ?>
-<!--                    --><?php //if ($item['parent_id'])?>
-<!--                        <div class="panel box box-success">-->
-<!--                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse_--><?//= $item['id']; ?><!--"-->
-<!--                               class="collapsed" aria-expanded="false">-->
-<!--                                --><?//= $item['title']; ?>
-<!--                            </a>-->
-<!---->
-<!--                            <div id="collapse_--><?//= $item['id']; ?><!--" class="panel-collapse collapse"-->
-<!--                                 aria-expanded="false">-->
-<!--                                --><?//= $item['description']; ?>
-<!--                                --><?php //if ($item['parent_id']): ?>
-<!--                                <a data-toggle="collapse" data-parent="#accordion1"-->
-<!--                                   href="--><?//= $item['parent_id'] != NULL ? '#capse_' . $item['id'] : 'а' ?><!--"-->
-<!--                                   aria-expanded="false"-->
-<!--                                   class="collapsed">-->
-<!--                                    --><?//= $item['title']; ?>
-<!--                                </a>-->
-<!--                                --><?php //endif; ?>
-<!--                                <div id="--><?//= $item['parent_id'] != NULL ? 'capse_' . $item['id'] : 'в' ?><!--"-->
-<!--                                     class="panel-collapse collapse"-->
-<!--                                     aria-expanded="true">-->
-<!--                                    --><?//= $item['description']; ?>
-<!--                                </div>-->
-<!--                            </div>-->
-<!---->
-<!--                                <div class="box-body">-->
-<!--                                    <div class="box-group" id="accordion1">-->
-<!---->
-<!---->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                        </div>-->
-<!--                    --><?php //endforeach; ?>
-<!--                </div>-->
-<!--            --><?php //endforeach; ?>
-
-
-<!--            --><?php //foreach ($regularitys as $regularity): ?>
-<!--            <div class="tab-pane --><?//= $regularity['id'] == 1 ? 'active' : '' ?><!--"-->
-<!--                 id="tab_--><?//= $regularity['id'] ?><!--">-->
-<!--                --><?php //foreach ($regularity->items as $item): ?>
-<!--                --><?php //if($item['regularity_id'] == $regularity['id']):?>
-<!---->
-<!--                <div class="box-body">-->
-<!--                    <div class="panel box box-success">-->
-<!---->
-<!--                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse_--><?//= $item['id']; ?><!--"-->
-<!--                           class="collapsed" aria-expanded="false">-->
-<!--                            --><?//= $item['title']; ?>
-<!--                        </a>-->
-<!--                        <div class="box-body">-->
-<!--                            <div id="collapse_--><?//= $item['id']; ?><!--" class="panel-collapse collapse"-->
-<!--                                 aria-expanded="false">-->
-<!--                                --><?//= $item['description']; ?>
-<!--<!--                            -->--><?php ////if($item['parent_id'] != null):?>
-<!---->
-<!--                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse_--><?//= $item['id']; ?><!--"-->
-<!--                                             class="collapsed" aria-expanded="false">-->
-<!--                                    --><?//= $item['title']; ?>
-<!--                                </a>-->
-<!--                                <div id="collapse_--><?//= $item['id']; ?><!--" class="panel-collapse collapse"-->
-<!--                                     aria-expanded="false">-->
-<!--                                    --><?//= $item['description']; ?>
-<!--                                </div>-->
-<!--<!--                            -->--><?php ////endif;?>
-<!--                            </div>-->
-<!--                        </div>-->
-<!--<!--                    -->--><?php ////else:?>
-<!---->
-<!---->
-<!---->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                --><?php //endif;?>
-<!--                --><?php //endforeach; ?>
-<!--            </div>-->
-<!--            --><?php //endforeach; ?>
-
-
-
-
-    <?php foreach ($regularitys as $regularity): ?>
-        <div class="tab-pane <?= $regularity['id'] == 1 ? 'active' : '' ?>"
-             id="tab_<?= $regularity['id'] ?>">
-            <?php foreach ($regularity->items as $item): ?>
-                <?php if($item['regularity_id'] == $regularity['id']):?>
-
-                    <div class="box-body">
-                        <div class="panel box box-success">
-                            <?php if($item['parent_id'] == null):?>
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse_<?= $item['id']; ?>"
-                               class="collapsed" aria-expanded="false">
-                                <?= $item['title']; ?>
-                            </a>
-                            <?php endif;?>
-                            <div class="box-body">
-                                <div id="collapse_<?= $item['id']; ?>" class="panel-collapse collapse"
-                                     aria-expanded="false">
-                                    <?= $item['description']; ?>
-                                    <?php if($item['parent_id'] != null):?>
-                        <a data-toggle="collapse" data-parent="#accordion1"
-                           href="<?= $item['parent_id'] != NULL ?? '#capse_' . $item['id']  ?>"
-                           aria-expanded="false"
-                           class="collapsed">
-                            <?= $item['title']; ?>
-                        </a>
-<!--                    --><?php //endif; ?>
-                    <div id="<?= $item['parent_id'] != NULL ?? 'capse_' . $item['id']  ?>"
-                         class="panel-collapse collapse"
-                         aria-expanded="true">
-                        <?= $item['description']; ?>
-                    </div>
-<!--                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse_--><?//= $item['id']; ?><!--"-->
-<!--                                       class="collapsed" aria-expanded="false">-->
-<!--                                        --><?//= $item['title']; ?>
-<!--                                    </a>-->
-<!--                                    <div id="collapse_--><?//= $item['id']; ?><!--" class="panel-collapse collapse"-->
-<!--                                         aria-expanded="false">-->
-<!--                                        --><?//= $item['description']; ?>
-<!--                                    </div>-->
-                                    <?php endif;?>
+            <?php foreach ($regularitys as $regularity): ?>
+                <div class="tab-pane <?= $regularity['id'] == 1 ? 'active' : '' ?>"
+                     id="tab_<?= $regularity['id'] ?>">
+                    <?php foreach ($regularity->items as $item) {
+                        if ($item['parent_id'] != null) {
+                            $data[] = $item;
+                        }
+                    }
+                    ?>
+                    <?php foreach ($regularity->items as $item): ?>
+                        <?php if (is_null($item['parent_id'])): ?>
+                            <div class="panel box box-danger">
+                                <div class="panel box box-border">
+                                    <h4 class="box-title">
+                                        <a data-toggle="collapse" data-parent="#accordion"
+                                           href="#collapse_<?= $item['id'] ?>" class="collapsed"
+                                           aria-expanded="false">
+                                            <?= $item['title']; ?>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div id="collapse_<?= $item['id'] ?>" class="panel-collapse collapse"
+                                     aria-expanded="false"
+                                     style="height: 0px;">
+                                    <div class="box-body">
+                                        <?= $item['description'] ?>
+                                        <?php foreach ($data as $value): ?>
+                                            <?php if ($value['parent_id'] == $item['id']): ?>
+                                                <div class="panel box box-primary">
+                                                    <div class="panel box box-primary">
+                                                        <h4>
+                                                            <a data-toggle="collapse" data-parent="#accordion"
+                                                               href="#capse_<?= $value['id'] ?>" class="collapsed"
+                                                               aria-expanded="false">
+                                                                <?= $value['title']; ?>
+                                                            </a>
+                                                        </h4>
+                                                    </div>
+                                                    <div id="capse_<?= $value['id'] ?>" class="panel-collapse collapse"
+                                                         aria-expanded="true">
+                                                        <div class="box-body">
+                                                            <?= $value['description']; ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            <?php endif; ?>
+                                        <?php endforeach; ?>
+                                    </div>
                                 </div>
                             </div>
-                            <!--                    --><?php //else:?>
-
-
-
-                        </div>
-                    </div>
-                <?php endif;?>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
+                </div>
             <?php endforeach; ?>
-        </div>
-    <?php endforeach; ?>
 
             <!-- /.tab-pane -->
         </div>
