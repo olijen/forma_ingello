ccbbacaf49f7057766f3baef47e145cc294c82e4
alex <aleksejemcenko1@gmail.com>
2021-09-17 19:38:08 +0300

    чек бар и кнопка удалить

--- forma/modules/selling/views/main/index.php
+++ forma/modules/selling/views/main/index.php
@@ -9,7 +9,8 @@ use forma\modules\customer\records\Customer;
 use forma\modules\warehouse\records\Warehouse;
 use forma\widgets\DateRangeFilter;
 use forma\modules\selling\records\state\State;
-use kartik\date\DatePicker;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $searchModel \forma\modules\selling\records\selling\SellingSearch */
@@ -36,35 +37,25 @@ $this->title = 'Продажи';
             'template' => '{update} {delete}',
         ],
         [
-            'attribute' => 'customerName',
-            'label' => 'Клиент',
+            'attribute' => 'customer_id',
             'value' => 'customer.name',
-        ],
-
-        [
-            'attribute' => 'customer_chief_phone',
-            'label' => 'Телефонный номер клиента',
-            'value' => 'customer.chief_phone'
-        ],
-        [
-            'attribute' => 'customer_telegram',
-            'label' => 'телеграм',
-            'value' => 'customer.telegram'
+            'filter' => ActiveRecordHelper::getListByQuery(
+                (new \forma\modules\customer\records\CustomerSearch())
+                    ->search(Yii::$app->request->queryParams)
+                    ->query,
+                'name'
+            ),
         ],
         [
-            'attribute' => 'customer_skype',
-            'label' => 'скайп',
-            'value' => 'customer.skype'
-        ],
-        [
-            'attribute' => 'customer_whatsapp',
-            'label' => 'вотсап',
-            'value' => 'customer.whatsapp'
-        ],
-        [
-            'attribute' => 'customer_viber',
-            'label' => 'вайбер',
-            'value' => 'customer.viber'
+            'attribute' => 'customer_id',
+            'label' => 'Компания',
+            'value' => 'customer.firm',
+            'filter' => ActiveRecordHelper::getListByQuery(
+                (new \forma\modules\customer\records\CustomerSearch())
+                    ->search(Yii::$app->request->queryParams)
+                    ->query,
+                'firm'
+            ),
         ],
         [
             'attribute' => 'warehouse_id',
@@ -81,52 +72,13 @@ $this->title = 'Продажи';
             'value' => 'toState.name',
             'filter' => ArrayHelper::map(State::find()->where(['user_id'=> Yii::$app->user->id])->all(),'id', 'name'),
         ],
-        [
-            'attribute' => 'next_step',
-            'label' => 'Следующий шаг',
-
-        ],
-
     ];
-    foreach (['date_next_step'] as $attribute) {
+    foreach (['date_create', 'date_complete'] as $attribute) {
         $columns[] = [
             'attribute' => $attribute,
-            'filter' => DatePicker::widget([
-                'name' => 'SellingSearch[date_next_step]',
-                'type' => DatePicker::TYPE_COMPONENT_PREPEND,
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format' => 'yyyy-mm-dd'
-                ],
-                'value' => isset($_GET['SellingSearch']['date_next_step']) ?
-                    $_GET['SellingSearch']['date_next_step'] : '',
-            ]),
-            'label' => 'Дата следующего шага'
-
+            'filter' => DateRangeFilter::widget(compact('attribute', 'searchModel')),
         ];
     }
-    foreach (['date_create'] as $attribute) {
-        $columns[] = [
-            'attribute' => $attribute,
-            'filter' => DatePicker::widget([
-                'name' => 'SellingSearch[date_create]',
-                'type' => DatePicker::TYPE_COMPONENT_PREPEND,
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format' => 'yyyy-mm-dd'
-                ],
-                'value' => isset($_GET['SellingSearch']['date_create']) ?
-                    $_GET['SellingSearch']['date_create'] : '',
-            ]),
-        ];
-    }
-
-
-    $columns[] =[
-        'attribute' => 'companyName',
-        'label' => 'Компания',
-        'value' => 'customer.firm',
-    ];
 
     echo DynaGrid::widget([
         'options' => ['id' => 'dyna-grid-' . $searchModel->tableName()],
@@ -136,12 +88,28 @@ $this->title = 'Продажи';
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
             'responsiveWrap' => false,
+            'toolbar' => [
+                [
+                    'content' => Html::button('<i class="glyphicon glyphicon-trash"></i> Удалить', [
+                        'type' => 'button',
+                        'class' => 'btn btn-danger forma_light_orange',
+                        'onclick' => '$("#grid-' . $searchModel->tableName() . '")
+                        .groupOperation("' . Url::to(['/product/product/delete-selection']) . '", {
+                            message: "Are you sure you want to delete selected items?"
+                        });
+                    ',
+                    ]),
+                ],
+                '{export}',
+                '{toggleData}',
+                '{dynagrid}',
+            ]
         ],
     ]);
 
     ?>
 
-    <?php  Pjax::end(); ?>
+    <?php Pjax::end(); ?>
 
 </div>
 
