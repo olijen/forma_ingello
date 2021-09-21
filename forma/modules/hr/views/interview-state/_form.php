f09fcf7065f64dd1d0bf52a4dd0cf4fe488e8b3c
Maksym Solomianyi <maxssolom@gmail.com>
2021-09-21 15:34:36 +0300

    Исправил баг

--- forma/modules/hr/views/interview-state/_form.php
+++ forma/modules/hr/views/interview-state/_form.php
@@ -9,18 +9,18 @@ use kartik\form\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 
 if($model->hasErrors()):
-\wokster\ltewidgets\BoxWidget::begin([
-'solid'=>true,
-'color'=>'danger',
-'title'=>'Ошибки валидации',
-'close'=> true,
-]);
-$error_data = $model->firstErrors;
-echo \yii\widgets\DetailView::widget([
-'model'=>$error_data,
-'attributes'=>array_keys($error_data)
-]);
-\wokster\ltewidgets\BoxWidget::end();
+    \wokster\ltewidgets\BoxWidget::begin([
+        'solid'=>true,
+        'color'=>'danger',
+        'title'=>'Ошибки валидации',
+        'close'=> true,
+    ]);
+    $error_data = $model->firstErrors;
+    echo \yii\widgets\DetailView::widget([
+        'model'=>$error_data,
+        'attributes'=>array_keys($error_data)
+    ]);
+    \wokster\ltewidgets\BoxWidget::end();
 endif;
 
 ?>
@@ -30,15 +30,10 @@ endif;
     <?php $form = ActiveForm::begin([
     ]); ?>
 
-            <?= $form->field($model, 'name', ['options'=>['class'=>'col-xs-12']])->textInput() ?>
+    <?= $form->field($model, 'name', ['options'=>['class'=>'col-xs-12']])->textInput() ?>
 
-            <?= $form->field($model, 'order', ['options'=>['class'=>'col-xs-12']])->textInput() ?>
 
-            <?= $form->field($model, 'description', ['options'=>['class'=>'col-xs-12']])->widget(Widget::className(), [
-                'settings' => [
-                    'lang' => 'ru',
-                    'minHeight' => 200,]])?>
-
+    <?= $form->field($model, 'order', ['options'=>['class'=>'col-xs-12']])->textInput() ?>
 
     <?= $form->field($model, 'description', ['options'=>['class'=>'col-xs-12']])->widget(Widget::className(), [
         'settings' => [
@@ -46,11 +41,10 @@ endif;
             'minHeight' => 200,]])?>
 
 
-
     <div class="col-xs-12 col-md-12">
-            <div class="form-group">
-                <?= Html::submitButton($model->isNewRecord ? 'Добавить' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-            </div>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Добавить' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
         </div>
+    </div>
     <?php ActiveForm::end(); ?>
 </div>
