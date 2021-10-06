7fe9ce5fbd60a117045b6cc3a8f242e5ebe513cd
Maksym Solomianyi <maxssolom@gmail.com>
2021-10-06 16:24:56 +0300

    Добавил метод getListVacancyProject, получаем массив вакансий-проектов в таком виде(вакансия|проект)

--- forma/modules/vacancy/records/Vacancy.php
+++ forma/modules/vacancy/records/Vacancy.php
@@ -4,12 +4,15 @@ namespace forma\modules\vacancy\records;
 
 use forma\components\AccessoryActiveRecord;
 use forma\components\EntityLister;
+use forma\modules\core\records\User;
 use forma\modules\hr\records\interview\Interview;
 use forma\modules\hr\records\interviewstate\InterviewState;
 use forma\modules\hr\records\interviewvacancy\InterviewVacancy;
 use forma\modules\project\records\project\Project;
 use forma\modules\project\records\projectvacancy\ProjectVacancy;
 use Yii;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "vacancy".
@@ -73,7 +76,37 @@ class Vacancy extends AccessoryActiveRecord
     {
         return EntityLister::getList(self::className(), $byUser);
     }
+    public static function getListVacancyProject()
+    {
+        $user = \Yii::$app->getUser()->getIdentity();
+        $ids = []; //$ids - это массив типа [1,2,3,4,5...]
+        $condition = '';
+        if ($user !== null) {
+            if ($user->parent_id != null) {
+                // Выбирает себя, реферера (начальника) и всех его рефералов (сотрудников)
+                $condition = "parent_id = {$user->parent_id} OR id = {$user->parent_id} or id = {$user->id}";
+            } else {
+                // Выбирает себя (начальника, реферера) и всех рефералов.
+                $condition = "parent_id = {$user->id} OR id = {$user->id}";
+            }
 
+            foreach (User::find()->where($condition)->all() as $user) {
+                array_push($ids, $user->id);
+            }
+        }
+
+        $projectVacancies = ProjectVacancy::find()->joinWith(['accessory'])
+            ->andWhere(['in', 'accessory.user_id', $ids])
+            ->andWhere(['accessory.entity_class' => ProjectVacancy::className()])->all();
+        $projectVacancyData = [];
+        foreach ($projectVacancies as $projectVacancy) {
+            $projectVacancyData[] = [
+                'id' => $projectVacancy->id,
+                'name' => $projectVacancy->vacancy->name . '|' . $projectVacancy->project->name
+            ];
+        }
+        return $projectVacancyData;
+    }
     /**
      * @return \yii\db\ActiveQuery
      */
@@ -98,33 +131,6 @@ class Vacancy extends AccessoryActiveRecord
         return $this->hasMany(Project::className(), ['id' => 'project_id'])->viaTable('project_vacancy', ['vacancy_id' => 'id']);
     }
 
-    public function getWorkerByProjectVacancyInterviewStateId($vacancy_id, $project_id, $state_id)
-    {
-        $interviewStates = InterviewState::find()->where(['user_id' => Yii::$app->user->id])->all();
-        $interviewStateIds = [];
-        foreach ($interviewStates as $interviewState) {
-            $interviewStateIds[] = $interviewState->id;
-        }
-        $interviews = Interview::find()
-            ->where(
-                [
-                    'vacancy_id' => $vacancy_id,
-                    'project_id' => $project_id,
-                ]
-            )
-            ->andWhere(['state_id' => $interviewStateIds])
-            ->all();
-        $workerByInterviewStates = [];
-        $workers = [];
-//        foreach ($interviews as $interview) {
-//            if (array_key_exists($interview->state_id, $workerByInterviewStates)) {
-//                array_push($workerByInterviewStates, $interview->worker_id);
-//            }
-//            $workerByInterviewStates[$interview->state_id] = $interview->worker_id;
-//        }
-//        dd($workerByInterviewStates);
-        return $workers;
-    }
     /**
      * {@inheritdoc}
      * @return VacancyQuery the active query used by this AR class.
