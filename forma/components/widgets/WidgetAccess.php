f0f25c4906089baba31a196de1563ff4925394db
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-11-05 16:16:56 +0200

    cancel widget access selling main

--- forma/components/widgets/WidgetAccess.php
+++ forma/components/widgets/WidgetAccess.php
@@ -7,39 +7,62 @@ use forma\modules\core\records\ItemInterface;
 use forma\modules\core\records\ItemRule;
 use yii\bootstrap\Widget;
 
-abstract class WidgetAccess extends Widget
+class WidgetAccess extends Widget
 {
-    public $id;
-
+    public $accessName;
+    public $rules;
     /**
      * @throws \yii\db\Exception
      */
     public function isAccessible(): bool
     {
-        $query = \Yii::$app->db->createCommand('select count(*) as answers from access_interface 
-                                                    where user_id ='.\Yii::$app->user->id.' and status =1 
-                                                    and rule_id in (select rule_id from item_rule 
-                                                    where item_interface_id='.$this->id.') having  
-                                                    count(*) = (select count(*) from item_rule 
-                                                    where item_interface_id='.$this->id.')')->queryOne();
 
-        if ($query == false) {
-            return false;
-        } else {
+        $query = ItemInterface::find()->where(['name_item' => $this->accessName])
+            ->joinWith(['itemRules' => function ($q) {
+                $q->joinWith(['rule' => function ($q) {
+                    $q->joinWith('accessInterfaces');
+                }]);
+            }])->all();
+        $userId = \Yii::$app->user->id;
+        $countItemRule = count($query[0]->itemRules);
+        $countUserAnswer = 0;
+        foreach ($query[0]->itemRules as $itemRule) {
+            $this->rules[] =$itemRule->rule->rule_name.' ';
+            foreach ($itemRule->rule->accessInterfaces as $accessInterface) {
+                if ($accessInterface->user_id == $userId && $accessInterface->status == 1) {
+                    $countUserAnswer++;
+
+                }
+            }
+        }
+        if ($countItemRule == $countUserAnswer) {
             return true;
+        } else {
+            return false;
         }
 
     }
 
-    abstract function printWidget();
+    public function init()
+    {
+        parent::init();
+        ob_start();
+    }
 
     /**
      * @throws \yii\db\Exception
      */
     public function run()
     {
+        $content = ob_get_clean();
         if ($this->isAccessible() == true) {
-            $this->printWidget();
+            echo $content;
+        } else {
+            echo "Выполните задание:<br/>";
+            foreach ($this->rules as $rule){
+                echo "$rule <br/>";
+            }
+            echo "После откроется доступ $this->accessName";
         }
     }
 
