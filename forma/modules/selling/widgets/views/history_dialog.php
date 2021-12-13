998f5fac301e9a73f0b02db58bf051b908f4ee88
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-12-13 11:19:55 +0200

    Правки по таску Страница продажи 1 часть

--- forma/modules/selling/widgets/views/history_dialog.php
+++ forma/modules/selling/widgets/views/history_dialog.php
@@ -3,12 +3,13 @@
 use yii\bootstrap\Html;
 use yii\widgets\Pjax;
 use yii\helpers\Url;
+use kartik\editors\Summernote;
 
 ?>
 <style>
     #chat{
         overflow-x: scroll;
-        height: 300px;
+        height: 250px;
     }
 </style>
 
@@ -41,7 +42,7 @@ use yii\helpers\Url;
             'name' => 'comment',
             'settings' => [
                 'lang' => 'ru',
-                //'minHeight' => 300,
+                'minHeight' => 220,
                 'plugins' => [
                     'clips',
                     'fullscreen',
@@ -59,11 +60,12 @@ use yii\helpers\Url;
                 'fileManagerJson' => \yii\helpers\Url::to(['/worker/worker/files-get']),
                 'fileUpload' => \yii\helpers\Url::to(['/worker/worker/file-upload'])
             ],
+            'options' => ['style'=>'overflow-x: scroll;']
         ]);
         ?>
     </div>
 
-    <div class="form-group col-md-12">
+    <div class="form-group col-md-12" style="padding: 0;">
 
         <button style="width: 100%;" type="submit" class="btn no-loader btn-success"><i class="fa fa-paper-plane"></i> Отправить</button>
 
