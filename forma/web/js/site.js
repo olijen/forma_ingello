2e8d4b2b03b6f54d78625fde66d61b53a39fc980
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2021-12-28 13:17:50 +0200

    todo select

--- forma/web/js/site.js
+++ forma/web/js/site.js
@@ -1,5 +1,4 @@
-
-
+//TODO вечать на grid-view select change
 $(function() {
     $('.grid-view tbody td, table.kv-grid-table tbody td').click(function(event) {
         if(!$(this).hasClass('kv-expand-icon-cell') && !$(this).hasClass('no-load') && !$(this).hasClass('no-load')){
