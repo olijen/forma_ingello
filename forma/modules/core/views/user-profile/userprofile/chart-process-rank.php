aa2b06d4cc9b0cd5e11299b1b04b5d8ea0339913
alex <aleksejemcenko1@gmail.com>
2022-01-05 14:55:34 +0200

    вывел значение на график

--- forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
+++ forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
@@ -1,10 +1,17 @@
+<?php
+
+use forma\modules\core\services\UserProfileChartService;
+
+$userProfileChart = new UserProfileChartService();
+
+?>
 <div class="col-md-4">
     <h1 style="text-align: center">График</h1>
 </div>
 
 <div class="box box-success">
     <div class="box-header with-border">
-        <h3 class="box-title" id="scrollllllllll">История изменений состояния продаж</h3>
+        <h3 class="box-title" id="scrol">График количества пройденных правил</h3>
 
         <div class="box-tools pull-right">
             <button type="button" class="btn btn-box-tool" data-widget="collapse">
@@ -20,29 +27,15 @@
         </div>
     </div>
 </div>
-<?php
-$date = strtotime('monday this week');
-$dates = '';
-for($i = 1;$i < 8;$i++) {
-$dates.= '\''.date("d.m.Y", $date).'\',';
-$date =  strtotime('+1 day', $date);
-}
-$count = \forma\modules\core\records\UserRuleRank::find()->where(['user_profile_id' => Yii::$app->user->id])
-    ->groupBy(['date'])->select(['date', 'count(id)'])->all()
-
-;
-dd($count);
-//    ->count();
-?>
 <script>
 
     myChart = new Chart(document.getElementById('myChart').getContext('2d'), {
         type: 'line',
         data: {
-            labels: [<?=$dates?>],
+            labels: [<?=$userProfileChart->getDate()?>],
             datasets: [{
                 label: 'Количество изменений',
-                data: [<?=$count?>],
+                data: [<?=$userProfileChart->getCount()?>],
                 backgroundColor: ['transparent'],
                 borderColor: ['green'],
             }]
