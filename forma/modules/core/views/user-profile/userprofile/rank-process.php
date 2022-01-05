24aac5da65c015bc6d0ab22ce885050880983a04
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2022-01-05 09:20:29 +0200

    создал блок вывода процесса прохождения испытаний

--- forma/modules/core/views/user-profile/userprofile/rank-process.php
+++ forma/modules/core/views/user-profile/userprofile/rank-process.php
@@ -19,22 +19,24 @@ use forma\modules\core\records\Rank;
     .social-links a i {
         font-size: 50px;
         line-height: 90px;
-        color: #909090;
+        color: black;
     }
 </style>
-<div class="col-md-4">
+<div class="col-md-4" style="min-height: 200px;">
     <h1 style="text-align: center">Процесс</h1>
     <div class="social-links" style="padding: 5px;">
         <?php
         foreach ($ranks as $rank) {
-            foreach ($rank->rules as $item) {
+            foreach ($rank->rules as $rule) {
+
                 ?>
-                <a title="<?= $item->rule_name ?>"><i class="fa <?= $item->icon ?>"></i></a>
+                <?php if ($rule->count_action == count($rule->userProfileRules)) { ?>
+                    <a style="background: green" title="<?= $rule->rule_name ?>"><i
+                                class="fa <?= $rule->icon ?>"></i></a>
+                <?php } else { ?>
+                    <a style="background: darkred" title="<?= $rule->rule_name ?>"><i class="fa <?= $rule->icon ?>"></i></a>
+                <?php } ?>
             <?php }
         } ?>
     </div>
 </div>
-<!--<a><i class="fa fa-user fa-lg"></i></a>
-                <a href="#"><i style="color: green;" class="fa fa-male fa-lg"></i></a>
-                <a href="#"><i class="fa fa-users fa-lg"></i></a>
-                <a href="#"><i class="fa fa-user-secret fa-lg"></i></a>-->
