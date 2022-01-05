afa2fc38c9c029e71fd7f7ba502a1d9da29f56be
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2022-01-05 16:25:18 +0200

    адаптировал блоки

--- forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
+++ forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
@@ -1,3 +1,13 @@
-<div class="col-md-4">
-    <h1 style="text-align: center">График</h1>
+<div class="col-md-4 col-sm-12 col-12 stretch-card">
+    <div class="box box-success">
+        <div class="box-header with-border big_widget_header">
+            <h3 class="box-title">
+                График
+            </h3>
+        </div>
+        <div class="box-body">
+            <h1 style="text-align: center">График</h1>
+        </div>
+    </div>
+
 </div>
