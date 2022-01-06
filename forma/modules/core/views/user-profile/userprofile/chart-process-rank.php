bca6b1ffebd07f24dabad6788f011f719369972f
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2022-01-06 09:11:32 +0200

    исправил вывод графика

--- forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
+++ forma/modules/core/views/user-profile/userprofile/chart-process-rank.php
@@ -5,7 +5,7 @@ use forma\modules\core\services\UserProfileChartService;
 $userProfileChart = new UserProfileChartService();
 
 ?>
-<div class="col-md-4 col-sm-12 col-12 stretch-card">
+<div class="col-md-6 col-sm-12 col-12 stretch-card">
     <div class="box box-success">
         <div class="box-header with-border big_widget_header">
             <h3 class="box-title">
@@ -13,29 +13,12 @@ $userProfileChart = new UserProfileChartService();
             </h3>
         </div>
         <div class="box-body">
-            <h1 style="text-align: center">График</h1>
-        </div>
-    </div>
-
-</div>
-
-<div class="box box-success">
-    <div class="box-header with-border">
-        <h3 class="box-title" id="scrol">График количества пройденных правил</h3>
-
-        <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                <i class="fa fa-minus"></i>
-            </button>
-        </div>
-    </div>
-    <div class="box-body">
-        <div class="chart">
             <div class="chart">
                 <canvas id="myChart" style=""></canvas>
             </div>
         </div>
     </div>
+
 </div>
 <script>
 
