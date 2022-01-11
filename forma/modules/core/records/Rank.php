d279176482a7bdb5cd02b9d8abc3c73cfd3424d3
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2022-01-11 16:33:00 +0200

    изменил таблицы

--- forma/modules/core/records/Rank.php
+++ forma/modules/core/records/Rank.php
@@ -5,28 +5,29 @@ namespace forma\modules\core\records;
 use Yii;
 
 /**
-* This is the model class for table "rank".
-*
-  * @property integer $id
-  * @property string $name
-  * @property string $image
-  * @property integer $order
-  *
-      * @property Rule[] $rules
-      * @property UserProfile[] $userProfiles
-  */
+ * This is the model class for table "rank".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $image
+ * @property integer $order
+ *
+ * @property ItemInterface[] $itemInterfaces
+ * @property Rule[] $rules
+ * @property UserProfile[] $userProfiles
+ */
 class Rank extends \yii\db\ActiveRecord
 {
     public $imageFile;
 
 
-  /**
-  * @inheritdoc
-  */
-  public static function tableName()
-  {
-    return 'rank';
-  }
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rank';
+    }
 
     /**
      * @inheritdoc
@@ -41,42 +42,50 @@ class Rank extends \yii\db\ActiveRecord
     }
 
 
-  /**
-  * @inheritdoc
-  */
-  public function attributeLabels()
-  {
-    return [
-        'id' => 'ID',
-        'name' => 'Name',
-        'image' => 'Image',
-        'imageFile' => 'Image',
-        'order' => 'Order',
-    ];
-  }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'image' => 'Image',
+            'imageFile' => 'Image',
+            'order' => 'Order',
+        ];
+    }
 
-  /**
-  * @return \yii\db\ActiveQuery
-  */
-  public function getRules()
-  {
-  return $this->hasMany(Rule::className(), ['rank_id' => 'id']);
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemInterfaces()
+    {
+        return $this->hasMany(ItemInterface::className(), ['rank_id' => 'id']);
+    }
 
-  /**
-  * @return \yii\db\ActiveQuery
-  */
-  public function getUserProfiles()
-  {
-  return $this->hasMany(UserProfile::className(), ['rank_id' => 'id']);
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRules()
+    {
+        return $this->hasMany(Rule::className(), ['rank_id' => 'id']);
+    }
 
-  /**
-  * @inheritdoc
-  * @return UserProfileRuleQuery the active query used by this AR class.
-  */
-  public static function find()
-  {
-  return new UserProfileRuleQuery(get_called_class());
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserProfiles()
+    {
+        return $this->hasMany(UserProfile::className(), ['rank_id' => 'id']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return UserProfileRuleQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new UserProfileRuleQuery(get_called_class());
+    }
 }
