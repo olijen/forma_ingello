82fdecdfd4107866cfa55e92338b76df99930694
alex <aleksejemcenko1@gmail.com>
2022-01-18 23:59:52 +0200

    test

---
+++ forma/training/cycle.php
@@ -0,0 +1,129 @@
+<?php
+set_time_limit(60);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+//for ($i = 1; $i<=100; $i++){
+//    echo $i . '<br>';
+//}
+
+//for ($i = 1; $i<=100; $i++){
+//    if ($i%2 == 0){
+//        echo $i;
+//    }
+//}
+//$count = 0;
+//for ($i = 1; $i<=100; $i++){
+//
+//    $count +=$i ;
+//}
+//echo $count;
+
+//$count = 0;
+//for ($i = 1; $i<=15; $i++){
+//
+//    $count +=pow($i,2) ;
+//}
+//echo $count;
+//
+//$count = 0;
+//for ($i = 1; $i<=15; $i++){
+//
+//    $count +=sqrt($i) ;
+//}
+////echo $count;
+//
+//$count = 0;
+//for ($i = 1; $i<=15; $i++){
+//
+//    $count +=round(sqrt($i),2) ;
+//}
+//echo $count;
+
+//$count = 0;
+//for ($i = 1; $i<=100; $i++){
+//
+//    if ($i%7 == 0){
+//
+//        echo $i;
+//    }
+//}
+//echo $count;
+//
+//$arr = [];
+//for ($i = 0; $i<=9; $i++){
+//
+//    $arr[$i] = 'x';
+//}
+//var_dump($arr);
+
+//$arr = [];
+//for ($i = 0; $i<=9; $i++){
+//
+//    $arr[$i] = $i+1;
+//}
+//var_dump($arr);
+//de(1);
+//
+//$arr = [];
+//for ($i = 10; $i>0; $i--){
+//
+//    $arr[$i] = $i;
+//}
+//var_dump($arr);
+//
+//$arr = [];
+//for ($i = 0; $i<=10; $i++){
+//
+//    $arr[$i] = rand(0,100);
+//}
+//var_dump($arr);
+//
+//$arr = '';
+//for ($i = 0; $i<=5; $i++){
+//
+//    $arr.= rand(0,9);
+//}
+//var_dump($arr);
+
+//$arr = '';
+//for ($i = 0; $i<=5; $i++){
+//
+//    $arr.= rand(0,9);
+//}
+//var_dump($arr);
+//
+//$arr = '';
+//for ($i = 0; $i<=5; $i++){
+//
+//    $arr.= rand(0,9);
+//}
+//var_dump($arr);
+
+//$arr = [];
+//for ($i = 0; $i<=9; $i++){
+//    $arr[$i]= rand(0,17);
+//}
+//$count = 0;
+//foreach ($arr as $item){
+//    $count += $item;
+//}
+//var_dump($count);
+
+//$count = 0;
+//foreach ($arr as $item){
+//    $count += $item*$item;
+//}
+//var_dump(floor(sqrt($count)));
+//var_dump($count);
+//$count = 0;
+//foreach ($arr as $item){
+//    if ($item>0 && $item<10){
+//        $count +=$item;
+//    }
+//}
+//echo $count;
+
+
+}
