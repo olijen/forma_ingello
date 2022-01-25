799b1c810721dc3d21830916fa64fc7df7d58a97
DimaNinyuk <dimaninyuk@gmail.com>
2022-01-25 14:38:18 +0200

    Revert "Исправить баг неправильного отображения таблицы"
    This reverts commit 68ed15d2be22af64676314afaab78359681000fb.

--- forma/modules/inventorization/widgets/views/remains-table.php
+++ forma/modules/inventorization/widgets/views/remains-table.php
@@ -44,7 +44,6 @@ echo GridView::widget([
     'tableKey' => 'remainsTable',
     'isEditable' => false,
     'useLocalStorage' => true,
-    'responsive' => false,
     'columns' => $columns,
     'dataProvider' => $dataProvider,
     'updateUrl' => Url::to(['/inventorization/state/confirm', 'id' => $model->id]),
