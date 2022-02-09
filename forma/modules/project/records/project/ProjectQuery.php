01f1d72436e776c0e5f5da72d2ccf8bd0878aaee
DimaNinyuk <dimaninyuk@gmail.com>
2022-02-09 09:37:09 +0200

    два бага в списках, убрать лишние записи

--- forma/modules/project/records/project/ProjectQuery.php
+++ forma/modules/project/records/project/ProjectQuery.php
@@ -2,6 +2,8 @@
 
 namespace forma\modules\project\records\project;
 
+use forma\components\AccessoryModule;
+
 /**
  * This is the ActiveQuery class for [[Project]].
  *
@@ -20,6 +22,12 @@ class ProjectQuery extends \yii\db\ActiveQuery
      */
     public function all($db = null)
     {
+        return parent::all($db);
+    }
+
+    public function allAccessory($db = null)
+    {
+        $this->andWhere(['in', 'project.id', AccessoryModule::getAccessoryIdS($this->modelClass)]);
         return parent::all($db);
     }
 
