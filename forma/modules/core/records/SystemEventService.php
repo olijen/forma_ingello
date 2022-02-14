5d778f4fbce7a41acaabc7450bebc687fce17cbd
DimaNinyuk <dimaninyuk@gmail.com>
2022-02-14 08:00:35 +0200

    Баг - при сохранении подписки на события вылазит ошибка

--- forma/modules/core/records/SystemEventService.php
+++ forma/modules/core/records/SystemEventService.php
@@ -22,6 +22,7 @@ class SystemEventService
         'SystemEvent',
         'Accessory',
         'StateSearchState',
+        'SystemEventUser',
         'TestSearch',
         'TestType',
         'TestTypeField',
