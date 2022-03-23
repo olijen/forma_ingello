0e3689ae3ce4d907f84b754b0413c1b0fd281612
DimaNinyuk <dimaninyuk@gmail.com>
2022-03-23 11:01:20 +0200

    Нужнодобавить валидацию при создании правила

--- forma/modules/core/records/Rule.php
+++ forma/modules/core/records/Rule.php
@@ -8,97 +8,98 @@ use forma\modules\core\records\ItemRule;
 use forma\modules\core\records\Item;
 
 /**
-* This is the model class for table "rule".
-*
-  * @property integer $id
-  * @property string $action
-  * @property string $table
-  * @property string $rule_name
-  * @property integer $count_action
-  * @property integer $item_id
-  *
-      * @property AccessInterface[] $accessInterfaces
-      * @property Item $item
-      * @property ItemRule $itemRules
-  */
+ * This is the model class for table "rule".
+ *
+ * @property integer $id
+ * @property string $action
+ * @property string $table
+ * @property string $rule_name
+ * @property integer $count_action
+ * @property integer $item_id
+ *
+ * @property AccessInterface[] $accessInterfaces
+ * @property Item $item
+ * @property ItemRule $itemRules
+ */
 class Rule extends AccessoryActiveRecord
 {
 
 
-  /**
-  * @inheritdoc
-  */
-  public static function tableName()
-  {
-    return 'rule';
-  }
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'rule';
+    }
 
-  /**
-  * @inheritdoc
-  */
-  public function behaviors()
-  {
-    return [
-          ];
-  }
-
-  /**
-  * @inheritdoc
-  */
-  public function rules()
-  {
-    return [
-            [['count_action', 'item_id'], 'integer'],
-            [['action', 'table','rule_name'], 'string', 'max' => 255],
-
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
         ];
-  }
+    }
 
-  /**
-  * @inheritdoc
-  */
-  public function attributeLabels()
-  {
-    return [
-        'id' => 'ID',
-        'action' => 'Событие',
-        'table' => 'Таблица',
-        'count_action' => 'Количество действий',
-        'rule_name'=>'Описание правила',
-        'item_id' => 'Item ID',
-    ];
-  }
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['item_id'], 'integer'],
+            [['action', 'table', 'rule_name'], 'string', 'max' => 255],
+            [['rule_name', 'table', 'action', 'count_action'], 'required'],
+            ['count_action', 'integer', 'min' => 1]
+        ];
+    }
 
-  /**
-  * @return \yii\db\ActiveQuery
-  */
-  public function getAccessInterfaces(): \yii\db\ActiveQuery
-  {
-  return $this->hasMany(AccessInterface::className(), ['rule_id' => 'id']);
-  }
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'action' => 'Событие',
+            'table' => 'Таблица',
+            'count_action' => 'Количество действий',
+            'rule_name' => 'Описание правила',
+            'item_id' => 'Item ID',
+        ];
+    }
 
-  /**
-  * @return \yii\db\ActiveQuery
-  */
-  public function getItemRule(): \yii\db\ActiveQuery
-  {
-  return $this->hasMany(ItemRule::className(), ['rule_id' => 'id']);
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAccessInterfaces(): \yii\db\ActiveQuery
+    {
+        return $this->hasMany(AccessInterface::className(), ['rule_id' => 'id']);
+    }
 
-  /**
-  * @return \yii\db\ActiveQuery
-  */
-  public function getItem()
-  {
-  return $this->hasOne(Item::className(), ['id' => 'item_id']);
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemRule(): \yii\db\ActiveQuery
+    {
+        return $this->hasMany(ItemRule::className(), ['rule_id' => 'id']);
+    }
 
-  /**
-  * @inheritdoc
-  * @return RuleQuery the active query used by this AR class.
-  */
-  public static function find()
-  {
-  return new RuleQuery(get_called_class());
-  }
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItem()
+    {
+        return $this->hasOne(Item::className(), ['id' => 'item_id']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return RuleQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new RuleQuery(get_called_class());
+    }
 }
