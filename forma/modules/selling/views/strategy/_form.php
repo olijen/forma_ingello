038a25d6e2f736acadfa7441314c07b608426433
DimaNinyuk <dimaninyuk@gmail.com>
2022-04-21 13:53:43 +0300

    Сделать возможность назначать  стратегиям   признак

--- forma/modules/selling/views/strategy/_form.php
+++ forma/modules/selling/views/strategy/_form.php
@@ -16,6 +16,18 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
+    <?php
+    if (isset($_GET['isSelling'])) {
+        $model->is_selling = $_GET['isSelling'];
+    }
+    ?>
+
+    <?= $form->field($model, 'is_selling')->widget(\kartik\select2\Select2::classname(), [
+        'data' => ['0' => 'Для найма', '1' => 'Для продаж'],
+        'value' => $model->is_selling ?? null,
+        'pluginOptions' => ['allowClear' => true],
+    ]) ?>
+
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Сохранить'), ['class' => 'btn btn-success']) ?>
     </div>
